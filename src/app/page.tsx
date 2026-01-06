7ed60d6010b738381954ca21a37faaf4300f2056
Hitury <xxhituryxx@gmail.com>
2026-01-06 15:59:26 +0100

    Fixed routing errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "./_components/ui/accordion"
+} from "@/src/_components/ui/accordion";
 
 export default function Home() {
   return (
