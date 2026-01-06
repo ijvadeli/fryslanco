b28f511b43b19faf7541cdf0e70fbea66c387f9e
ijvadeli <idhvandelindt@icloud.com>
2026-01-06 16:47:13 +0100

    Removed some things and fixed some errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/src/_components/ui/accordion";
-
 import { Header } from "@/src/_components/header";
 
 export default function Home() {
   return (
     <>
     <Header />
-    <Accordion type="single" collapsible>
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
     </>
   );
 }
