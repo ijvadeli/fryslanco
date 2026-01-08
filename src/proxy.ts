93e432c6b7391568d80b4aa97c4776210ff7754b
Hitury <xxhituryxx@gmail.com>
2026-01-08 17:22:10 +0100

    Proxy fixes, changed publishable key to anon key

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,14 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
-export async function updateSession(request: NextRequest) {
+export default async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   })
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    process.env.NEXT_SUPABASE_URL!,
+    process.env.NEXT_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
